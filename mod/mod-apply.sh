25e190943db88e0b68f2f8fddae423a4ab7e3b9c
Null <pairmanxlr@gmail.com>
2025-04-16 10:36:04 +0800

    Update mod-apply.sh

--- mod/mod-apply.sh
+++ mod/mod-apply.sh
@@ -6,8 +6,8 @@ KERNEL_CONF=build/boards/sg200x/sg2002_licheervnano_sd/linux/sg2002_licheervnano
 mv -v "$SDK_CONF" "$SDK_CONF.bak"
 mv -v "$KERNEL_CONF" "$KERNEL_CONF.bak"
 
-mv -v "mod/$SDK_CONF" "$SDK_CONF"
-mv -v "mod/$KERNEL_CONF" "$KERNEL_CONF"
+cp -v "mod/$SDK_CONF" "$SDK_CONF"
+cp -v "mod/$KERNEL_CONF" "$KERNEL_CONF"
 
 popd
 
